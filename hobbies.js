9cc581ef559129105c53496ebeed3738e50abbbc
maurizio-dominici <mauriziodominici4@gmail.com>
2025-05-08 15:43:31 +0200

    add function in hobbies.js and export

--- hobbies.js
+++ hobbies.js
@@ -1,1 +1,9 @@
+const getHobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+  const hobbies = [`${hobbyOne}`, `${hobbyTwo}`, `${hobbyThree}`];
 
+  return hobbies;
+};
+
+module.exports = {
+  getHobbies: getHobbies,
+};
